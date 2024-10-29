0333ae2ef111964a9ad70557735cfa2cf47bb141
tbuckworth <elkilesh@hotmail.co.uk>
2024-10-29 16:11:35 +0000

    added IPL2

--- inverse_reward_shaping.py
+++ inverse_reward_shaping.py
@@ -6,8 +6,8 @@ import einops
 #%%
 n_states = 10
 n_actions = 5
-deterministic = False
-sparse_reward = False
+deterministic = True
+sparse_reward = True
 
 #%%
 if deterministic:
@@ -75,8 +75,38 @@ def soft_q_value_iteration(T, R, gamma, n_iterations=1000):
     print('soft value iteration did not converge after', n_iterations, 'iterations')
         
     
-soft_Q, soft_V, soft_pi = soft_q_value_iteration(T, true_R, gamma)
+soft_Q, soft_V, soft_pi = soft_q_value_iteration(T, true_R, gamma, 10000)
 
+# %%
+def soft_q_value_iteration_with_pi(T, R, gamma, n_iterations=1000):
+    Q = torch.zeros(n_states, n_actions)
+    V = torch.zeros(n_states)
+    # normal method
+
+    for i in range(n_iterations):
+        old_Q = Q
+        V = Q.logsumexp(dim=-1)
+        PI = Q.softmax(dim=-1)
+        Q = einops.einsum(PI, T, gamma * V,
+                          'states actions, states actions next_states, next_states -> states actions') + R.unsqueeze(1)
+
+        if (Q - old_Q).abs().max() < 1e-10:
+            print(f'soft value iteration with pi converged in {i} iterations')
+            pi = Q.softmax(dim=1)
+            return Q, V, pi
+    print('soft value iteration did not converge after', n_iterations, 'iterations')
+
+
+pi_Q, pi_V, pi_pi = soft_q_value_iteration_with_pi(T, true_R, gamma, 10000)
+
+soft_A = soft_Q - soft_V.unsqueeze(-1)
+pi_A = pi_Q - pi_V.unsqueeze(-1)
+torch.allclose(soft_A, pi_A)
+torch.corrcoef(torch.stack((pi_pi.flatten(),soft_pi.flatten())))
+
+
+
+# %%
 
 #%%
 def evaluate_policy(T, R, gamma, pi, n_iterations=1000):
@@ -142,18 +172,23 @@ plt.show()
 
 # %%
 def implicit_policy_learning(T, R, gamma, n_iterations=30000):
-    PI = torch.zeros(n_states, n_actions, requires_grad=True)
+    Q = torch.zeros(n_states, n_actions, requires_grad=True)
     V = torch.randn(n_states, requires_grad=True)
     R = R.unsqueeze(-1)
     R.requires_grad = False
-    optimizer = torch.optim.Adam([PI,V], lr=1e-3)
+    optimizer = torch.optim.Adam([Q,V], lr=1e-3)
     for i in range(n_iterations):
-        A = PI.softmax(dim=-1).log()
-        R_hat = A + V.unsqueeze(1) - gamma *einops.einsum(T, V, "states actions next_states, next_states -> states actions")
+        old_Q = Q.detach().clone()
+        PI = Q.softmax(dim=-1)
+        A = PI.log()
+        R_hat = A + V.unsqueeze(1) - gamma *einops.einsum(PI, T, V, "states actions, states actions next_states, next_states -> states actions")
         loss = ((R_hat - R)**2).mean()
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
+        if (Q - old_Q).abs().max() < 1e-5:
+            print(f'implicit policy learning converged in {i} iterations')
+            return A, V
     return A, V
 
 learned_A, learned_V = implicit_policy_learning(T, true_R, gamma)
@@ -196,3 +231,46 @@ hard_A = torch.nn.functional.one_hot(soft_A.argmax(dim=-1)).float()
 
 learned_R_opt, _ = inverse_reward_shaping(T, hard_A, gamma)
 torch.corrcoef(torch.stack((true_R,learned_R_opt)))
+
+# %%
+def implicit_policy_learning2(T, R, gamma, alpha=0.1, n_iterations=10000):
+    Q = torch.randn(n_states, n_actions, requires_grad=True)
+    F = torch.randn(n_states, n_actions, requires_grad=True)
+    R = R.unsqueeze(-1)
+    R.requires_grad = False
+    optimizer = torch.optim.Adam([Q, F], lr=1e-3)
+    corr_list = []
+    for i in range(n_iterations):
+        PI = (Q/alpha).softmax(dim=-1)
+        A = PI.log()
+        R_hat = Q - gamma * F
+        Qn = einops.einsum(PI, T, Q, "states actions, states actions next_states, next_states actions -> states next_states actions")
+        Vn = Qn.exp().sum(dim=-2).log()#.unsqueeze(-1) # ?
+        # R_hat = A + V.unsqueeze(1) - gamma *einops.einsum(T, V, "states actions next_states, next_states -> states actions")
+        loss1 = ((R_hat - R)**2).mean()
+        loss2 = ((Vn-F)**2).mean()
+        loss = loss1 + loss2
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        correlation = torch.corrcoef(torch.stack((A.flatten(), soft_A.flatten())))[0, 1].item()
+        corr_list.append(correlation)
+        if i % 1000 == 0:
+            print(correlation)
+            plt.scatter(A.exp().flatten().detach().cpu().numpy(), soft_A.exp().flatten().detach().cpu().numpy())
+            plt.show()
+    return A, corr_list
+
+learned_A, corr_list = implicit_policy_learning2(T, true_R, gamma, 0.1, 100000)
+
+torch.corrcoef(torch.stack((A.exp().flatten(), soft_A.exp().flatten())))[0, 1].item()
+
+plt.scatter(learned_A.exp().flatten().detach().cpu().numpy(), soft_A.exp().flatten().detach().cpu().numpy())
+plt.show()
+
+plt.plot(corr_list)
+plt.show()
+
+
+
+# %%
