9393a06c60039123feae37b280b739837ad31c5e
tbuckworth <elkilesh@hotmail.co.uk>
2024-11-20 14:48:22 +0000

    PPOing canonicaliser

--- run_canon.py
+++ run_canon.py
@@ -3,7 +3,17 @@ from hyperparameter_optimization import run_next_hyperparameters
 
 if __name__ == '__main__':
     hparams = {
-        "model_file": "logs/train/maze_aisc/maze1/2024-11-11__20-51-51__seed_1080/model_200015872.pth",
+        # coinrun unshifted
+        # "model_file":  "logs/train/coinrun/coinrun/2024-10-05__17-20-34__seed_6033/model_200015872.pth",
+
+        # coinrun shifted
+        # "model_file":  "logs/train/coinrun/coinrun/2024-10-05__18-06-44__seed_6033/model_200015872.pth",
+
+        #unshifted maze:
+        "model_file":  "logs/train/maze_aisc/maze1/2024-11-08__15-54-16__seed_1080/model_200015872.pth",
+
+        #shifted maze:
+        # "model_file": "logs/train/maze_aisc/maze1/2024-11-11__20-51-51__seed_1080/model_200015872.pth",
         "epoch": 0,
         "algo": "canon",
         "env_name": "get",
@@ -13,7 +23,7 @@ if __name__ == '__main__':
         "num_checkpoints": 1,
         "use_wandb": True,
         "num_timesteps": int(65000),
-        "val_epoch": 200,
+        "val_epoch": 30,
         "mini_batch_size": 2048,
         # "learning_rate": 1e-3,
     }
