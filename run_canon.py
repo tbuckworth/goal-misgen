e1d1340319949368c3a217724cfb79c499444963
tbuckworth <elkilesh@hotmail.co.uk>
2024-11-20 14:52:10 +0000

    doing coinrun canon

--- run_canon.py
+++ run_canon.py
@@ -2,29 +2,32 @@
 from hyperparameter_optimization import run_next_hyperparameters
 
 if __name__ == '__main__':
-    hparams = {
-        # coinrun unshifted
-        # "model_file":  "logs/train/coinrun/coinrun/2024-10-05__17-20-34__seed_6033/model_200015872.pth",
+    for model_file in ["logs/train/coinrun/coinrun/2024-10-05__18-06-44__seed_6033/model_200015872.pth",
+                       "logs/train/coinrun/coinrun/2024-10-05__17-20-34__seed_6033/model_200015872.pth"]:
+        hparams = {
+            "model_file": model_file,
+            # coinrun unshifted
+            # "model_file":  "logs/train/coinrun/coinrun/2024-10-05__17-20-34__seed_6033/model_200015872.pth",
 
-        # coinrun shifted
-        # "model_file":  "logs/train/coinrun/coinrun/2024-10-05__18-06-44__seed_6033/model_200015872.pth",
+            # coinrun shifted
+            # "model_file":  "logs/train/coinrun/coinrun/2024-10-05__18-06-44__seed_6033/model_200015872.pth",
 
-        #unshifted maze:
-        "model_file":  "logs/train/maze_aisc/maze1/2024-11-08__15-54-16__seed_1080/model_200015872.pth",
+            #unshifted maze:
+            # "model_file":  "logs/train/maze_aisc/maze1/2024-11-08__15-54-16__seed_1080/model_200015872.pth",
 
-        #shifted maze:
-        # "model_file": "logs/train/maze_aisc/maze1/2024-11-11__20-51-51__seed_1080/model_200015872.pth",
-        "epoch": 0,
-        "algo": "canon",
-        "env_name": "get",
-        "exp_name": "maze1",
-        "param_name": "ascent-canon",
-        "wandb_tags": ["maze1 canon"],
-        "num_checkpoints": 1,
-        "use_wandb": True,
-        "num_timesteps": int(65000),
-        "val_epoch": 30,
-        "mini_batch_size": 2048,
-        # "learning_rate": 1e-3,
-    }
-    run_next_hyperparameters(hparams)
+            #shifted maze:
+            # "model_file": "logs/train/maze_aisc/maze1/2024-11-11__20-51-51__seed_1080/model_200015872.pth",
+            "epoch": 0,
+            "algo": "canon",
+            "env_name": "get",
+            "exp_name": "maze1",
+            "param_name": "ascent-canon",
+            "wandb_tags": ["coinrun canon"],
+            "num_checkpoints": 1,
+            "use_wandb": True,
+            "num_timesteps": int(65000),
+            "val_epoch": 30,
+            "mini_batch_size": 2048,
+            # "learning_rate": 1e-3,
+        }
+        run_next_hyperparameters(hparams)
