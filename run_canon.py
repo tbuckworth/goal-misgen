f1b247928fe191af74f21450648f1767f9b8dd9b
tbuckworth <elkilesh@hotmail.co.uk>
2024-11-21 10:29:34 +0000

    added validation to value_loss in canonicaliser. made the cononicalisation in the non-validation environment use the policy's value function.

--- run_canon.py
+++ run_canon.py
@@ -3,9 +3,10 @@ from hyperparameter_optimization import run_next_hyperparameters
 
 if __name__ == '__main__':
     for model_file in [
-        # "logs/train/maze_aisc/maze1/2024-11-08__15-54-16__seed_1080/model_200015872.pth",
-        "logs/train/coinrun/coinrun/2024-10-05__18-06-44__seed_6033/model_200015872.pth",
-        "logs/train/coinrun/coinrun/2024-10-05__17-20-34__seed_6033/model_200015872.pth"
+        "logs/train/maze_aisc/maze1/2024-11-08__15-54-16__seed_1080/model_200015872.pth",
+        "logs/train/maze_aisc/maze1/2024-11-11__20-51-51__seed_1080/model_200015872.pth",
+        # "logs/train/coinrun/coinrun/2024-10-05__18-06-44__seed_6033/model_200015872.pth",
+        # "logs/train/coinrun/coinrun/2024-10-05__17-20-34__seed_6033/model_200015872.pth"
     ]:
         hparams = {
             "model_file": model_file,
@@ -29,8 +30,10 @@ if __name__ == '__main__':
             "num_checkpoints": 1,
             "use_wandb": True,
             "num_timesteps": int(65000),
-            "val_epoch": 1000,
+            "val_epoch": 200,
             "mini_batch_size": 2048,
+            "n_val_envs": 128,
+            "n_envs": 256+128,
             # "learning_rate": 1e-3,
         }
         run_next_hyperparameters(hparams)
