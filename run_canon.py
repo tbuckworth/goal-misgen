b5f83597e877f4193bc336c2df971ec6c48856b5
tbuckworth <elkilesh@hotmail.co.uk>
2024-12-05 11:59:51 +0000

    modified canon method to estimate advantage

--- run_canon.py
+++ run_canon.py
@@ -142,7 +142,7 @@ def hp_run(model_file):
 
 
 if __name__ == '__main__':
-    ignore_errors = False
+    ignore_errors = True
     for model_file in reversed(unique_ascent_dirs):
         if not ignore_errors:
             hp_run(model_file)
