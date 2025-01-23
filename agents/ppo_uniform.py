ea169d8c5db2fb58f76b9bfbd72637ce170761d6
tbuckworth <elkilesh@hotmail.co.uk>
2025-01-23 12:32:04 +0000

    created ppo_uniform

---
+++ agents/ppo_uniform.py
@@ -0,0 +1,71 @@
+from torch.distributions import Categorical
+import torch
+import numpy as np
+from .ppo import PPO
+
+
+class PPO_Uniform(PPO):
+    def __init__(self,
+                 *args,
+                 **kwargs):
+        super(PPO_Uniform, self).__init__(args, kwargs)
+
+
+    def predict(self, obs, hidden_state, done):
+        with torch.no_grad():
+            obs = torch.FloatTensor(obs).to(device=self.device)
+            hidden_state = torch.FloatTensor(hidden_state).to(device=self.device)
+            mask = torch.FloatTensor(1-done).to(device=self.device)
+            dist, value, hidden_state = self.policy(obs, hidden_state, mask)
+            p = (torch.ones_like(dist.probs, dtype=torch.float32, device=self.device)/dist.probs.shape(-1))
+            dist = Categorical(probs=p)
+            act = dist.sample()
+            log_prob_act = dist.log_prob(act)
+
+        return act.cpu().numpy(), log_prob_act.cpu().numpy(), value.cpu().numpy(), hidden_state.cpu().numpy()
+
+    def optimize(self):
+        pi_loss_list, value_loss_list, entropy_loss_list, l1_reg_list, total_loss_list = [], [], [], [], []
+        batch_size = self.n_steps * self.n_envs // self.mini_batch_per_epoch
+        if batch_size < self.mini_batch_size:
+            self.mini_batch_size = batch_size
+        grad_accumulation_steps = batch_size / self.mini_batch_size
+        grad_accumulation_cnt = 1
+
+        self.policy.train()
+        for e in range(self.epoch):
+            recurrent = self.policy.is_recurrent()
+            generator = self.storage.fetch_train_generator(mini_batch_size=self.mini_batch_size,
+                                                           recurrent=recurrent)
+            for sample in generator:
+                obs_batch, hidden_state_batch, act_batch, done_batch, \
+                    old_log_prob_act_batch, old_value_batch, return_batch, adv_batch = sample
+                mask_batch = (1-done_batch)
+                dist_batch, value_batch, _ = self.policy(obs_batch, hidden_state_batch, mask_batch)
+
+                # Clipped Bellman-Error
+                clipped_value_batch = old_value_batch + (value_batch - old_value_batch).clamp(-self.eps_clip, self.eps_clip)
+                v_surr1 = (value_batch - return_batch).pow(2)
+                v_surr2 = (clipped_value_batch - return_batch).pow(2)
+                value_loss = 0.5 * torch.max(v_surr1, v_surr2).mean()
+
+                # Policy Entropy
+                loss = self.value_coef * value_loss
+                loss.backward()
+
+                # Let model to handle the large batch-size with small gpu-memory
+                if grad_accumulation_cnt % grad_accumulation_steps == 0:
+                    torch.nn.utils.clip_grad_norm_(self.policy.parameters(), self.grad_clip_norm)
+                    self.optimizer.step()
+                    self.optimizer.zero_grad()
+                grad_accumulation_cnt += 1
+                value_loss_list.append(value_loss.item())
+
+        summary = {
+            'Loss/total': np.mean(total_loss_list),
+            'Loss/pi': np.mean(pi_loss_list),
+            'Loss/v': np.mean(value_loss_list),
+            'Loss/entropy': np.mean(entropy_loss_list),
+            'Loss/l1_reg': np.mean(l1_reg_list)
+        }
+        return summary
