0dfbedfb432a55937d572a5d0cbee84ed4151688
tbuckworth <elkilesh@hotmail.co.uk>
2025-01-30 16:04:06 +0000

    creating multi-train threads for run_canon.py

--- ptrain.sh
+++ ptrain.sh
@@ -11,4 +11,5 @@ TERM=vt100
 /usr/bin/nvidia-smi
 export CUDA_DIR=/vol/cuda/12.2.0/:${CUDAPATH}
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=/vol/cuda/12.2.0/
-python3.8 /vol/bitbucket/${USER}/goal-misgen/multi_train.py $1
+which multi_canon.py
+#python3.8 /vol/bitbucket/${USER}/goal-misgen/multi_train.py $1
