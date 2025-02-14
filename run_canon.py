3714e89481ce4460f8fde086a9bd2f7ec98b4c13
tbuckworth <elkilesh@hotmail.co.uk>
2025-02-14 17:04:00 +0000

    created kldiv meg

--- run_canon.py
+++ run_canon.py
@@ -220,7 +220,7 @@ def hp_run(model_file, tag_dict, tag):
         "num_checkpoints": 1,
         "use_wandb": True,
         "num_timesteps": int(67584),
-        "val_epoch": 100,
+        "val_epoch": 300,
         "mini_batch_size": 2048,
         "n_val_envs": 16,
         "n_envs": int(64 + 16),
@@ -326,6 +326,6 @@ if __name__ == '__main__':
     # # model_files = maze_dirs + new_maze_dirs
     # run_tags_for_files({"Maze_VOrig_Soft_Inf": None}, new_maze_dirs[1:], ignore_errors=True)
     #
-    run_tags_for_files({"Ascent_Meg_KL0":None}, local_unique_ascent_dirs, ignore_errors=False)
+    run_tags_for_files({"Ascent_Meg_KL1":None}, local_unique_ascent_dirs, ignore_errors=False)
     # #
     # # run_tags_for_files({"Coinrun_Soft_Inf": None}, coinrun_dirs, ignore_errors=True)
