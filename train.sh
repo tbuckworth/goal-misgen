13da5dd16a8006a1880a15d605c8fe70640c3ac5
tbuckworth <elkilesh@hotmail.co.uk>
2025-03-27 08:17:00 +0000

    running maze

--- train.sh
+++ train.sh
@@ -12,6 +12,6 @@ TERM=vt100
 export CUDA_DIR=/vol/cuda/12.2.0/:${CUDAPATH}
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=/vol/cuda/12.2.0/
 #python3.8 /vol/bitbucket/${USER}/goal-misgen/train.py --exp_name coinrun --env_name coinrun --num_levels 100000 --distribution_mode hard --param_name hard-500 --num_timesteps 200000000 --num_checkpoints 5 --seed 6033 --use_wandb
-#python3.8 /vol/bitbucket/${USER}/goal-misgen/train.py --exp_name maze1 --env_name maze_aisc --num_levels 500 --distribution_mode hard --param_name hard-500 --num_timesteps 200000000 --num_checkpoints 5 --seed 42 --use_wandb
+python3.8 /vol/bitbucket/${USER}/goal-misgen/train.py --exp_name maze1 --env_name maze_aisc --num_levels 500 --distribution_mode hard --param_name hard-500 --num_timesteps 200000000 --num_checkpoints 5 --seed 100 --use_wandb
 #python3.8 /vol/bitbucket/${USER}/goal-misgen/train.py --exp_name cartpole --env_name cartpole --param_name cartpole-mlp --num_timesteps 10000000 --num_checkpoints 1 --seed 6033 --use_wandb
-python3.8 /vol/bitbucket/${USER}/goal-misgen/run_train_dict.py
+#python3.8 /vol/bitbucket/${USER}/goal-misgen/run_train_dict.py
