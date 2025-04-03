7eb28d3d4662829f1cf6c5e6f88441a5fcaa42ec
tbuckworth <elkilesh@hotmail.co.uk>
2025-04-03 18:20:22 +0100

    added importance sampling baselines

--- run_canon.py
+++ run_canon.py
@@ -333,7 +333,7 @@ if __name__ == '__main__':
 
     # # model_files = maze_dirs + new_maze_dirs
     # run_tags_for_files({"Maze_VOrig_Soft_Inf": None}, maze_dirs_apr25, ignore_errors=True)
-    run_tags_for_files({"test importance sampling": None}, maze_dirs_apr25, ignore_errors=False)
+    run_tags_for_files({"Maze_Imp_Sampling": None}, maze_dirs + new_maze_dirs + maze_dirs_apr25, ignore_errors=False)
 
     #
     # run_tags_for_files({"Ascent_Meg_KL8_gamma":None}, local_unique_ascent_dirs, ignore_errors=True)
