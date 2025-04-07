9f399bc718f220b0734ed715b8a9dd6ac9d12cc9
tbuckworth <elkilesh@hotmail.co.uk>
2025-04-07 12:08:11 +0100

    running cartpole trusted value

--- run_train_dict.py
+++ run_train_dict.py
@@ -2,6 +2,7 @@ from helper_local import get_model_with_largest_checkpoint
 from hyperparameter_optimization import run_next_hyperparameters
 
 if __name__ == '__main__':
+    # coinrun/maze:
     hparams = {
         "num_levels": 100000,
         "architecture": "impala",
@@ -20,6 +21,7 @@ if __name__ == '__main__':
         "trusted_policy_name": "tempered_gen",
         "td_lmbda": False,
     }
+    #cartpole:
     hparams = {
         # "num_levels": 100000,
         "architecture": "mlpmodel",
@@ -41,6 +43,30 @@ if __name__ == '__main__':
         "td_lmbda": False,
         # TODO: have fewer hidden dims?
         "hidden_dims": [256, 256, 256, 256],
+        "save_pics_ascender": False,
+        "trusted_policy_name": "tempered_gen",
+    }
+    #Ascent:
+    hparams = {
+        # "num_levels": 100000,
+        "architecture": "mlpmodel",
+        # "distribution_mode": "hard",
+        "seed": 1080,
+        "env_name": "ascent",
+        "exp_name": "value",
+        "param_name": "trusted-value",
+        "wandb_tags": ["trusted value"],
+        "algo": "trusted-value",
+        "num_timesteps": int(128 * 762),
+        "use_wandb": True,
+        "val_epoch": 1000,
+        "mini_batch_size": 2048,
+        "n_val_envs": 256,
+        "n_envs": 512 + 256,
+        "n_steps": 128,
+        "n_pos_states": 10,
+        "td_lmbda": False,
+        "hidden_dims": [16, 16],
         "save_pics_ascender": False,
         "trusted_policy_name": "tempered_gen",
     }
