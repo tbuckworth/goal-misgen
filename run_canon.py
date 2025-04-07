abe6116d55eb9c419efa00e17a06f2f61ae84192
tbuckworth <elkilesh@hotmail.co.uk>
2025-04-07 21:04:53 +0100

    figuring out correct trusted temp

--- run_canon.py
+++ run_canon.py
@@ -338,7 +338,7 @@ if __name__ == '__main__':
 
     # # model_files = maze_dirs + new_maze_dirs
     # run_tags_for_files({"Maze_VOrig_Soft_Inf": None}, maze_dirs_apr25, ignore_errors=True)
-    run_tags_for_files({"new maze tempered loaded": None}, maze_dirs + new_maze_dirs + maze_dirs_apr25, ignore_errors=False)
+    run_tags_for_files({"new maze tempered corrected": None}, maze_dirs + new_maze_dirs + maze_dirs_apr25, ignore_errors=False)
 
     #
     # run_tags_for_files({"Ascent_Meg_KL8_gamma":None}, local_unique_ascent_dirs, ignore_errors=True)
