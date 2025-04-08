f0cd852a1d37d922093f60ecaae3c931eb226285
tbuckworth <elkilesh@hotmail.co.uk>
2025-04-08 15:54:58 +0100

    running run_canon ascent

--- run_canon.py
+++ run_canon.py
@@ -232,12 +232,12 @@ def hp_run(model_file, tag_dict, tag):
         "wandb_tags": [tag],  # "pre-trained-value"],  # "coinrun misgen3"],
         "num_checkpoints": 1,
         "use_wandb": True,
-        "num_timesteps": int(2048*40),#int(67584),
-        "val_epoch": 1500,
+        "num_timesteps": int(384*256),#int(67584),
+        "val_epoch": 3000,
         "mini_batch_size": 2048,
-        "n_val_envs": 8,
-        "n_envs": int(32 + 8),
-        "n_steps": 2048,#256,#1000
+        "n_val_envs": 128,
+        "n_envs": int(256 + 128),
+        "n_steps": 256,#256,#1000
         "num_levels": 10000,
         "distribution_mode": "hard",
         "seed": seed,
@@ -342,7 +342,7 @@ if __name__ == '__main__':
     # run_tags_for_files({"new maze double tempered": None}, maze_dirs + new_maze_dirs + maze_dirs_apr25, ignore_errors=False)
 
     #
-    run_tags_for_files({"new ascent tempered3":None}, unique_ascent_dirs, ignore_errors=True)
+    run_tags_for_files({"new ascent tempered4":None}, unique_ascent_dirs, ignore_errors=True)
     # run_tags_for_files({"new cartpole double tempered":None}, cartpole_dirs, ignore_errors=True)
 
     # #
