990cc855d81eac01929da913180b22fdfaf1058d
tbuckworth <elkilesh@hotmail.co.uk>
2025-04-08 08:19:46 +0100

    running run_canon ascent

--- run_canon.py
+++ run_canon.py
@@ -342,9 +342,9 @@ if __name__ == '__main__':
     # run_tags_for_files({"new maze double tempered": None}, maze_dirs + new_maze_dirs + maze_dirs_apr25, ignore_errors=False)
 
     #
-    # run_tags_for_files({"new ascent double tempered":None}, unique_ascent_dirs, ignore_errors=True)
+    run_tags_for_files({"new ascent double tempered":None}, unique_ascent_dirs, ignore_errors=True)
     # run_tags_for_files({"new cartpole double tempered":None}, cartpole_dirs, ignore_errors=True)
 
     # #
     # run_tags_for_files({"Coinrun_Soft_Inf": None}, new_coinrun_dirs, ignore_errors=True)
-    run_tags_for_files({"new coinrun double tempered": None}, coinrun_dirs + new_coinrun_dirs, ignore_errors=True)
+    # run_tags_for_files({"new coinrun double tempered": None}, coinrun_dirs + new_coinrun_dirs, ignore_errors=True)
