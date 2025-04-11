c40a58cd38023f19a4eadf3c48727a08534d15d0
tbuckworth <elkilesh@hotmail.co.uk>
2025-04-11 13:02:36 +0100

    making a few changes to canonicalisation. Namely target.detach() for value function

--- run_canon.py
+++ run_canon.py
@@ -341,10 +341,10 @@ if __name__ == '__main__':
 
     # # model_files = maze_dirs + new_maze_dirs
     # run_tags_for_files({"Maze_VOrig_Soft_Inf": None}, maze_dirs_apr25, ignore_errors=True)
-    # run_tags_for_files({"Maze_VOrig_Soft_Target": None}, maze_dirs , ignore_errors=True)
-    # run_tags_for_files({"Maze_VOrig_Soft": None}, new_maze_dirs, ignore_errors=True)
-    # run_tags_for_files({"Maze_VOrig_Soft": None}, maze_dirs_apr25, ignore_errors=True)
-    run_tags_for_files({"Test": None}, maze_dirs + new_maze_dirs + maze_dirs_apr25, ignore_errors=True)
+    run_tags_for_files({"Maze_VOrig_Soft_Target_Mean_Adj": None}, maze_dirs , ignore_errors=True)
+    # run_tags_for_files({"Maze_VOrig_Soft_Target_Mean_Adj": None}, new_maze_dirs, ignore_errors=True)
+    # run_tags_for_files({"Maze_VOrig_Soft_Target_Mean_Adj": None}, maze_dirs_apr25, ignore_errors=True)
+    # run_tags_for_files({"Test": None}, maze_dirs + new_maze_dirs + maze_dirs_apr25, ignore_errors=True)
 
     #
     # run_tags_for_files({"new ascent uniform no inf":None}, local_unique_ascent_dirs, ignore_errors=False)
