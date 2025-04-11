048cb0b17d9859cb2b5764d0a840c8203142603a
tbuckworth <elkilesh@hotmail.co.uk>
2025-04-11 13:05:07 +0100

    making a few changes to canonicalisation. Namely target.detach() for value function

--- run_canon.py
+++ run_canon.py
@@ -342,9 +342,9 @@ if __name__ == '__main__':
 
     # # model_files = maze_dirs + new_maze_dirs
     # run_tags_for_files({"Maze_VOrig_Soft_Inf": None}, maze_dirs_apr25, ignore_errors=True)
-    # run_tags_for_files({"Maze_VOrig_Soft_Target_Mean_Adj": None}, maze_dirs , ignore_errors=True)
+    run_tags_for_files({"Maze_VOrig_Soft_Target_Mean_Adj": None}, maze_dirs , ignore_errors=True)
     # run_tags_for_files({"Maze_VOrig_Soft_Target_Mean_Adj": None}, new_maze_dirs, ignore_errors=True)
-    run_tags_for_files({"Maze_VOrig_Soft_Target_Mean_Adj": None}, maze_dirs_apr25, ignore_errors=True)
+    # run_tags_for_files({"Maze_VOrig_Soft_Target_Mean_Adj": None}, maze_dirs_apr25, ignore_errors=True)
     # run_tags_for_files({"Test": None}, maze_dirs + new_maze_dirs + maze_dirs_apr25, ignore_errors=True)
 
     #
