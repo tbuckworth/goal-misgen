a54676528b17389faa13b7f78cb50fa2f8936b5f
tbuckworth <elkilesh@hotmail.co.uk>
2025-05-07 10:02:56 +0100

    testing lots of different canon versions

--- multi_train.py
+++ multi_train.py
@@ -49,6 +49,20 @@ def main():
         # Distribute the same venv_python path to each parallel worker
         pool.starmap(run_canon, args)
 
+def hp():
+    if len(sys.argv) < 2:
+        print("Usage: python multi_train.py <n>")
+        print("Example: python multi_train.py 5")
+        sys.exit(1)
+
+    n = int(sys.argv[1])  # Number of parallel processes
+
+    args = [(i,) for i in range(n)]
+    # Verify that the venv's python exists
+
+    with multiprocessing.Pool(processes=n) as pool:
+        # Distribute the same venv_python path to each parallel worker
+        pool.starmap(run_script, args)
 
 if __name__ == "__main__":
-    main()
+    hp()
