817c05d3966d369eb1da863c9763b4140d1d860c
tbuckworth <elkilesh@hotmail.co.uk>
2025-05-07 17:35:15 +0100

    trying Ascent_Hard_Mean_Adj_Target

--- multi_canon_local.py
+++ multi_canon_local.py
@@ -4,9 +4,9 @@ from run_canon import run_tags_for_files
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_files', type=str, nargs='+', default=[], help='subject_policy_dirs')
+    parser.add_argument('--model_files', type=str, help='subject_policy_dirs')
     parser.add_argument('--tag', type=str, default='Test', help='Unique Identifier Wandb Tag')
     args = parser.parse_args()
 
-    run_tags_for_files({args.tag: None}, args.model_files, ignore_errors=True)
+    run_tags_for_files({args.tag: None}, args.model_files.split(" "), ignore_errors=True)
 
