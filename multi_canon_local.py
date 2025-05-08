88b17c4eac365135ec0dcd26bf5ac33595e469da
tbuckworth <elkilesh@hotmail.co.uk>
2025-05-08 09:04:24 +0100

    trying Ascent_Hard_Mean_Adj_Target

--- multi_canon_local.py
+++ multi_canon_local.py
@@ -4,7 +4,7 @@ from run_canon import run_tags_for_files
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_files', type=str, help='subject_policy_dirs')
+    parser.add_argument('--model_files', type=str, nargs='+', help='subject_policy_dirs')
     parser.add_argument('--tag', type=str, default='Test', help='Unique Identifier Wandb Tag')
     args = parser.parse_args()
 
--- multi_train.py
+++ multi_train.py
@@ -25,11 +25,11 @@ def run_canon(_, model_files, tag):
     model_files = [base_dir + m for m in model_files]
     # subprocess.run(["/vol/bitbucket/tfb115/goal-misgen/opvenv/bin/python3.8", f"multi_canon.py --model_files {' '.join(model_files)} --tag {tag}"])
     cmd = ["/vol/bitbucket/tfb115/goal-misgen/opvenv/bin/python3.8",
-           "multi_canon_local.py",
+           "/vol/bitbucket/tfb115/goal-misgen/multi_canon_local.py",
            "--tag",
            tag,
            "--model_files",
-           " ".join(model_files),
+           *model_files,
            ]
     subprocess.run(cmd)
 
