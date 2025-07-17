20968ecb978cc4b5ce922486735e0af3e80bad93
Rohan Mathew <rm1723@vertex07.doc.ic.ac.uk>
2025-07-17 13:50:25 +0100

    feat: :zap: use training queue to further parallelise maze

--- run_maze_gpuc.sh
+++ run_maze_gpuc.sh
@@ -2,7 +2,7 @@ sbatch --export=rand_region=0,seed=6033,num_levels=1000 gpuc_train_maze.sh
 sbatch --export=rand_region=0,seed=6033,num_levels=100000 gpuc_train_maze.sh
 sbatch --export=rand_region=0,seed=42,num_levels=1000 gpuc_train_maze.sh
 sbatch --export=rand_region=0,seed=42,num_levels=100000 gpuc_train_maze.sh
-sbatch --export=rand_region=10,seed=6033,num_levels=1000 gpuc_train_maze.sh
-sbatch --export=rand_region=10,seed=6033,num_levels=100000 gpuc_train_maze.sh
-sbatch --export=rand_region=10,seed=42,num_levels=1000 gpuc_train_maze.sh
-sbatch --export=rand_region=10,seed=42,num_levels=100000 gpuc_train_maze.sh
+sbatch --partition training --export=rand_region=10,seed=6033,num_levels=1000 gpuc_train_maze.sh
+sbatch --partition training --export=rand_region=10,seed=6033,num_levels=100000 gpuc_train_maze.sh
+sbatch --partition training --export=rand_region=10,seed=42,num_levels=1000 gpuc_train_maze.sh
+sbatch --partition training --export=rand_region=10,seed=42,num_levels=100000 gpuc_train_maze.sh
