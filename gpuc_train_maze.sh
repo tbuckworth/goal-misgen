9a0b62762e2f8b901b82c05ca2f9eb8ca093941e
Rohan Mathew <rm1723@vertex07.doc.ic.ac.uk>
2025-07-17 10:41:57 +0100

    feat: :sparkles: gpu cluster deploy scripts

---
+++ gpuc_train_maze.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=24G
+#SBATCH --gres=gpu:1
+#SBATCH --time=48:00:00
+#SBATCH --job-name=canon
+
+module load CMake/3.18.4-GCCcore-10.2.0
+module load Qt5/5.15.2-GCCcore-10.3.0
+module load Python/3.8.6-GCCcore-10.2.0
+
+# Set Qt5 directory for CMake
+export Qt5_DIR=$EBROOTQT5/lib/cmake/Qt5
+
+cd /vol/bitbucket/$USER/goal-misgen/
+source /vol/bitbucket/tfb115/goal-misgen/venv/bin/activate
+# python -m pip install --upgrade pip
+# pip install -r requirements.txt
+# pip install -e ../procgenAISC/
+python3.8 train.py --exp_name maze_aisc --env_name maze_aisc --num_levels $num_levels --distribution_mode hard --param_name hard-500 --num_timesteps 200000000 --num_checkpoints 5 --seed $seed --use_wandb --rand_region $rand_region
+
+#usage:
+# sbatch --export=rand_region=10,num_levels=100000,seed=42 hpc_train_maze.sh
---
+++ run_maze_gpuc.sh
@@ -0,0 +1,8 @@
+sbatch --export=rand_region=0,seed=6033,num_levels=1000 gpuc_train_maze.sh
+sbatch --export=rand_region=0,seed=6033,num_levels=100000 gpuc_train_maze.sh
+sbatch --export=rand_region=0,seed=42,num_levels=1000 gpuc_train_maze.sh
+sbatch --export=rand_region=0,seed=42,num_levels=100000 gpuc_train_maze.sh
+sbatch --export=rand_region=10,seed=6033,num_levels=1000 gpuc_train_maze.sh
+sbatch --export=rand_region=10,seed=6033,num_levels=100000 gpuc_train_maze.sh
+sbatch --export=rand_region=10,seed=42,num_levels=1000 gpuc_train_maze.sh
+sbatch --export=rand_region=10,seed=42,num_levels=100000 gpuc_train_maze.sh
