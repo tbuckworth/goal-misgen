38e13e9a554ad0615dcc45abf8ecc3832d723ae4
Rohan Mathew <rm1723@vertex07.doc.ic.ac.uk>
2025-07-17 13:57:49 +0100

    fix: :bug: testing if dependency install works on gpu

--- gpuc_train_maze.sh
+++ gpuc_train_maze.sh
@@ -16,10 +16,11 @@ export Qt5_DIR=$EBROOTQT5/lib/cmake/Qt5
 
 # TODO hardcoded my username since slurm does not seem to have USER env var
 cd /vol/bitbucket/rm1723/goal-misgen/
-source /vol/bitbucket/tfb115/goal-misgen/venv/bin/activate
-# python -m pip install --upgrade pip
-# pip install -r requirements.txt
-# pip install -e ../procgenAISC/
+# source /vol/bitbucket/tfb115/goal-misgen/venv/bin/activate
+source venv/bin/activate
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+pip install -e ../procgenAISC/
 python3.8 train.py --exp_name maze_aisc --env_name maze_aisc --num_levels $num_levels --distribution_mode hard --param_name hard-500 --num_timesteps 200000000 --num_checkpoints 5 --seed $seed --use_wandb --rand_region $rand_region
 
 #usage:
