bcbe7cc2856cf9f82ae17cb9b32215b47c67fa55
tbuckworth <elkilesh@hotmail.co.uk>
2025-07-21 09:14:17 +0100

    modified ppo slightly

--- hyperparameter_optimization.py
+++ hyperparameter_optimization.py
@@ -204,7 +204,7 @@ def ppo():
         # "entropy_coef": [0, 0.02, 0.1, 0.2],
         "l1_coef": 0,
         "anneal_lr": False,
-        # "reward_termination": "get",
+        "reward_termination": "get",
         # "reward_termination": 495,
         "train_pct_ood": 0,#[0, 0, 0, 0, 0.01, 0.1, 0.25, 0.5],
         "alpha_max_ent": [0.0, 0.5, 1., 2.],
