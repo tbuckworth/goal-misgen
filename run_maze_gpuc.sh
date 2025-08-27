885062a6ca17403d213aabed370c86db15b391f6
Rohan Mathew <rm1723@vertex07.doc.ic.ac.uk>
2025-08-27 13:47:41 +0100

    updated maze looped deployment

--- run_maze_gpuc.sh
+++ run_maze_gpuc.sh
@@ -1,8 +1,15 @@
-sbatch --export=rand_region=0,seed=6033,num_levels=1000 gpuc_train_maze.sh
-sbatch --export=rand_region=0,seed=6033,num_levels=100000 gpuc_train_maze.sh
-sbatch --export=rand_region=0,seed=42,num_levels=1000 gpuc_train_maze.sh
-sbatch --export=rand_region=0,seed=42,num_levels=100000 gpuc_train_maze.sh
-sbatch --partition training --export=rand_region=10,seed=6033,num_levels=1000 gpuc_train_maze.sh
-sbatch --partition training --export=rand_region=10,seed=6033,num_levels=100000 gpuc_train_maze.sh
-sbatch --partition training --export=rand_region=10,seed=42,num_levels=1000 gpuc_train_maze.sh
-sbatch --partition training --export=rand_region=10,seed=42,num_levels=100000 gpuc_train_maze.sh
+#!/bin/bash
+
+# Define your lists
+rand_regions=(0 10)
+seeds=(6033 42 902 1034)
+num_levels_list=(1000 100000)
+
+# Nested loops to generate all combinations
+for rand_region in "${rand_regions[@]}"; do
+    for seed in "${seeds[@]}"; do
+        for num_levels in "${num_levels_list[@]}"; do
+            sbatch --export=rand_region=${rand_region},seed=${seed},num_levels=${num_levels} gpuc_train_maze.sh
+        done
+    done
+done
--- run_maze_hpc.sh
+++ run_maze_hpc.sh
@@ -1,8 +1,15 @@
-qsub -v rand_region=0,seed=6033,num_levels=1000 hpc_train_maze.sh
-qsub -v rand_region=0,seed=6033,num_levels=100000 hpc_train_maze.sh
-qsub -v rand_region=0,seed=42,num_levels=1000 hpc_train_maze.sh
-qsub -v rand_region=0,seed=42,num_levels=100000 hpc_train_maze.sh
-qsub -v rand_region=10,seed=6033,num_levels=1000 hpc_train_maze.sh
-qsub -v rand_region=10,seed=6033,num_levels=100000 hpc_train_maze.sh
-qsub -v rand_region=10,seed=42,num_levels=1000 hpc_train_maze.sh
-qsub -v rand_region=10,seed=42,num_levels=100000 hpc_train_maze.sh
+#!/bin/bash
+
+# Define your lists
+rand_regions=(0 10)
+seeds=(6033 42 902 1034)
+num_levels_list=(1000 100000)
+
+# Nested loops to generate all combinations
+for rand_region in "${rand_regions[@]}"; do
+    for seed in "${seeds[@]}"; do
+        for num_levels in "${num_levels_list[@]}"; do
+            qsub -v rand_region=${rand_region},seed=${seed},num_levels=${num_levels} hpc_train_maze.sh
+        done
+    done
+done
