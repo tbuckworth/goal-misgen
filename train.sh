a81b21632c822e778e9886f32bcb815a4d3e32fd
tbuckworth <elkilesh@hotmail.co.uk>
2025-09-02 11:11:16 +0100

    increased depth

--- train.sh
+++ train.sh
@@ -11,7 +11,7 @@ TERM=vt100
 /usr/bin/nvidia-smi
 export CUDA_DIR=/vol/cuda/12.2.0/:${CUDAPATH}
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=/vol/cuda/12.2.0/
-python3.8 /vol/bitbucket/tfb115/goal-misgen/train.py hyperparameter_optimization.py
+python3.8 /vol/bitbucket/tfb115/goal-misgen/hyperparameter_optimization.py
 #--exp_name coinrun --env_name coinrun --num_levels 1000 --distribution_mode hard --param_name hard-500 --num_timesteps 200000000 --num_checkpoints 5 --seed 6033 --use_wandb --model_file logs/train/coinrun/coinrun/2025-01-24__15-27-41__seed_6033/model_200015872.pth --extra_overrides learning_rate=0.0
 # python3.8 /vol/bitbucket/tfb115/goal-misgen/train.py --exp_name coinrun --env_name coinrun --num_levels 100000 --distribution_mode hard --param_name hard-500 --num_timesteps 200000000 --num_checkpoints 5 --seed 0 --use_wandb --model_file logs/train/coinrun/coinrun/2025-01-28__11-36-54__seed_0/model_200015872.pth
 
